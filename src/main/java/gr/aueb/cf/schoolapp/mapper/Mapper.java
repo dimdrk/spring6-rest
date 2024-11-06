b0989c07877c8d7062f64991d60aa28440d87873
DimDrk <dimdrk@gmail.com>
2024-11-06 14:26:45 +0200

    mapToTeacherReadOnlyDTO

--- src/main/java/gr/aueb/cf/schoolapp/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/schoolapp/mapper/Mapper.java
@@ -1,8 +1,6 @@
 package gr.aueb.cf.schoolapp.mapper;
 
-import gr.aueb.cf.schoolapp.dto.PersonalInfoInsertDTO;
-import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
-import gr.aueb.cf.schoolapp.dto.UserInsertDTO;
+import gr.aueb.cf.schoolapp.dto.*;
 import gr.aueb.cf.schoolapp.model.PersonalInfo;
 import gr.aueb.cf.schoolapp.model.Teacher;
 import gr.aueb.cf.schoolapp.model.User;
@@ -14,6 +12,29 @@ import org.springframework.stereotype.Component;
 public class Mapper {
 
     // private final PasswordEncoder passwordEncoder;
+
+    public TeacherReadOnlyDTO mapToTeacherReadOnlyDTO(Teacher teacher) {
+        var dto = new TeacherReadOnlyDTO();
+
+        dto.setId(teacher.getId());
+        dto.setUuid(teacher.getUuid());
+        dto.setActive(teacher.isActive());
+
+        // mat to UserReadOnlyDTO
+        var userDTO = new UserReadOnlyDTO();
+        userDTO.setFirstname(teacher.getUser().getFirstname());
+        userDTO.setLastname(teacher.getUser().getLastname());
+        userDTO.setVat(teacher.getUser().getVat());
+        dto.setUser(userDTO);
+
+        // map to PersonalInfoDTO
+        var personalInfoDTO = new PersonalInfoReadOnlyDTO();
+        personalInfoDTO.setAmka(teacher.getPersonalInfo().getAmka());
+        personalInfoDTO.setIdentityNumber(teacher.getPersonalInfo().getIdentityNumber());
+        dto.setPersonalInfo(personalInfoDTO);
+
+        return dto;
+    }
 
     public Teacher mapToTeacherEntity(TeacherInsertDTO teacherInsertDTO) {
         Teacher teacher = new Teacher();
