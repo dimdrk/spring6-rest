3bc1eb798ee4ce288007660a5c715849b33517d4
DimDrk <dimdrk@gmail.com>
2024-11-08 21:11:27 +0200

    SecurityConfiguration

--- src/main/java/gr/aueb/cf/schoolapp/security/SecurityConfiguration.java
+++ src/main/java/gr/aueb/cf/schoolapp/security/SecurityConfiguration.java
@@ -3,8 +3,10 @@ package gr.aueb.cf.schoolapp.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,5 +30,11 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+            throws Exception {
+        return config.getAuthenticationManager();
     }
 }
